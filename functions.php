19b095fa70c05d0abdd1cbb0ba93f42d163b9fb8
Cristina <cristinna.iulia@gmail.com>
2021-05-04 08:52:13 +0300

    removed min style

--- functions.php
+++ functions.php
@@ -23,7 +23,7 @@ add_action('wp_enqueue_scripts', 'dlm_styles');
 function dlm_scripts() {
 	wp_dequeue_style( 'storefront-style' );
 	wp_deregister_style( 'storefront-style' );
-	wp_enqueue_style( 'storefront-style', get_stylesheet_directory_uri() . '/assets/css/style.' . ( ( WP_DEBUG ) ? '' : 'min.' ) . 'css', array(), '2.2.0' );
+	wp_enqueue_style( 'storefront-style', get_stylesheet_directory_uri() . '/assets/css/style.css', array(), '2.2.0' );
 	wp_enqueue_script( 'dlm-sticky-sidebar-js', get_stylesheet_directory_uri() . '/assets/js/sticky-sidebar.js', array( 'jquery' ), true );
 	if (is_page_template('template-pricing-page.php')) {
 		wp_enqueue_script( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js', array( 'jquery' ) );
