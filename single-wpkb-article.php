fb8ec0a44dda01f58b176e2da6237f69b6152260
diana-dumbravean <70516873+diana-dumbravean@users.noreply.github.com>
2021-07-20 11:18:19 +0300

    single article page

--- single-wpkb-article.php
+++ single-wpkb-article.php
@@ -6,26 +6,47 @@
        'number' => 1,
        'parent' => 0
    ]);
-
 ?>
 
 <section class="pt-8 pt-md-11 bg-light">
-<?php while ( have_posts() ) : the_post(); ?>
     <div class="container">
-        <div class="row align-items-center">
-            <h1 class="display-4 mb-2">
-                <?php the_title(); ?>
-            </h1>
-            <hr class="my-6 my-md-8 border-gray-300">
+        <div class="row">
+            <div class="col-12 col-md-3">
+                <div class="sidebar card shadow-light-lg">
+                    <?php
+                    $terms = get_terms([
+                        'taxonomy' => 'wpkb-category',
+                        'hide_empty' => false,
+                        'parent' => 0
+                    ]);
+                    ?>
+                    <h4 class="m-5">Categories</h4>
+                    <?php foreach ($terms as $term): ?>
+                    <a href="<?php echo get_term_link($term) ?>">
+                        <ul class="list-unstyled ms-5">
+                            <li class=" text-gray-800">
+                                <?php echo $term->name; ?>
+                                <hr class="border-gray">
+                            </li>
+                        </ul>
+                    </a>
+                    <?php endforeach ?>
+                </div>
+            </div>
+            <div class="col-12 col-md-8">
+            <?php while ( have_posts() ) : the_post(); ?>
+                <h1 class=" col-12 col-md-8display-4 mb-2">
+                    <?php the_title(); ?>
+                </h1>
+                <hr class="my-6 my-md-8 border-gray-300">
+                <div class="">
+                    <p class="text-gray-800 mb-6 mb-md-8">
+                    <?php the_content(); ?>
+                    </p>
+                </div>  
+            </div>
+            <?php endwhile; // end of the loop. ?> 
         </div>
     </div>
-    <div class="row">
-        <div class="col-12 col-md-8">
-            <p>
-                <?php echo term_description(); ?>
-            </p>
-        </div>
-    </div>  
-    <?php endwhile; // end of the loop. ?> 
 </section>
 <?php get_footer(); ?>
