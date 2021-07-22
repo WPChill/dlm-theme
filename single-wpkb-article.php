adc653adbe0015533da4a8ba48e35821038cfb49
diana-dumbravean <70516873+diana-dumbravean@users.noreply.github.com>
2021-07-22 14:40:59 +0300

    Homepage and categories updates

--- single-wpkb-article.php
+++ single-wpkb-article.php
@@ -1,32 +1,26 @@
-<?php get_header();
-
-       $terms = get_terms([
-       'taxonomy' => 'wpkb-category',
-       'hide_empty' => false,
-       'number' => 1,
-       'parent' => 0
-   ]);
-?>
-
-<section class="pt-8 pt-md-11 bg-light">
+<?php 
+$terms = get_terms([
+    'taxonomy' => 'wpkb-category',
+    'hide_empty' => false,
+    'parent' => 0
+]);
+$count = 0;
+get_header(); ?>
+<body id="article">
+<section class="pt-8 pb-8 pt-md-11">
     <div class="container">
         <div class="row">
             <div class="col-12 col-md-3">
                 <div class="sidebar card shadow-light-lg">
-                    <?php
-                    $terms = get_terms([
-                        'taxonomy' => 'wpkb-category',
-                        'hide_empty' => false,
-                        'parent' => 0
-                    ]);
-                    ?>
                     <h4 class="m-5">Categories</h4>
-                    <?php foreach ($terms as $term): ?>
+                    <?php foreach ($terms as $term): $count++ ?>
                     <a href="<?php echo get_term_link($term) ?>">
                         <ul class="list-unstyled ms-5">
                             <li class=" text-gray-800">
                                 <?php echo $term->name; ?>
+                                <?php if ( $count != count($terms) ): ?>
                                 <hr class="border-gray">
+                                <?php endif; ?>
                             </li>
                         </ul>
                     </a>
@@ -35,7 +29,7 @@
             </div>
             <div class="col-12 col-md-8">
             <?php while ( have_posts() ) : the_post(); ?>
-                <h1 class=" col-12 col-md-8display-4 mb-2">
+                <h1 class=" col-12 col-md-8 mb-2">
                     <?php the_title(); ?>
                 </h1>
                 <hr class="my-6 my-md-8 border-gray-300">
