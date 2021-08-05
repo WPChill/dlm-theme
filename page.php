3669b2e670dba04d53bfe83c16f0b797be6fa571
razvanaldea89 <44024114+razvanaldea89@users.noreply.github.com>
2021-08-05 13:10:04 +0300

    #9 remove extra $class; #8 closing div problem

--- page.php
+++ page.php
@@ -4,18 +4,9 @@
  *
  * @package wpchill-theme
  */
-
 ?>
 <?php get_header(); ?>
-<?php get_template_part( 'template-parts/element', 'page-header' );
-$class = '';
-if ( is_checkout() ) {
-	$class = 'checkout-header';
-} elseif ( is_account_page() ) {
-	$class = 'myaccount-header';
-}
-
-?>
+<?php get_template_part( 'template-parts/element', 'page-header' ); ?>
 
 <div id="main" class="main">
 	<div class="container">
@@ -24,45 +15,45 @@ if ( is_checkout() ) {
 			while ( have_posts() ) :
 				the_post();
 				?>
-		<article>
+				<article>
 
-			<section class="pt-8 pt-md-11 <?php echo esc_html( $class ) ?>">
-				<div class="container">
-					<div class="row justify-content-center">
-						<div class="col-12 col-md-10 col-lg-9 col-xl-8">
-						<?php if ( has_post_thumbnail() ) : ?>
-							<figure class="figure mb-7">
+					<section class="pt-8 pt-md-11">
+						<div class="container">
+							<div class="row justify-content-center">
+								<div class="col-12 col-md-10 col-lg-9 col-xl-8">
+									<?php if ( has_post_thumbnail() ) : ?>
+										<figure class="figure mb-7">
 
-								<!-- Image -->
-								<img class="figure-img img-fluid rounded lift lift-lg" src="<?php echo esc_attr( get_the_post_thumbnail_url() ); ?>" >
+											<!-- Image -->
+											<img class="figure-img img-fluid rounded lift lift-lg"
+												 src="<?php echo esc_attr( get_the_post_thumbnail_url() ); ?>">
 
-								<!-- Caption -->
-								<figcaption class="figure-caption text-center">
-									<?php echo esc_html( get_the_post_thumbnail_caption() ); ?>
-								</figcaption>
+											<!-- Caption -->
+											<figcaption class="figure-caption text-center">
+												<?php echo esc_html( get_the_post_thumbnail_caption() ); ?>
+											</figcaption>
 
-							</figure>
-						<?php endif; ?>
-							<!-- Heading -->
+										</figure>
+									<?php endif; ?>
+									<!-- Heading -->
 
-							<?php echo the_title( '<h1 class="display-4 text-center">', '</h1>' ); // phpcs:ignore ?>
+									<?php echo the_title( '<h1 class="display-4 text-center">', '</h1>' ); // phpcs:ignore
+									?>
 
-						</div>
-					</div> <!-- / .row -->
-				</div> <!-- / .container -->
-			</section>
+								</div>
+							</div> <!-- / .row -->
+						</div> <!-- / .container -->
+					</section>
 
-			<!-- Content -->
-			<?php get_template_part( 'template-parts/element', 'page-content' ); ?>
+					<!-- Content -->
+					<?php get_template_part( 'template-parts/element', 'page-content' ); ?>
 
-		</article>
-
-	</div>
-</div>
-		<?php
+				</article>
+			<?php
 			endwhile;
-		};
+		}
 		?>
-
+	</div><!-- / .container -->
+</div> <!-- / .main -->
 
 <?php get_footer(); ?>
