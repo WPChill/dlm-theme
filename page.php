023645372935a9500e382a2657ea4948f2f4458f
razvanaldea89 <44024114+razvanaldea89@users.noreply.github.com>
2021-08-05 13:43:43 +0300

    #7 container class

--- page.php
+++ page.php
@@ -9,51 +9,52 @@
 <?php get_template_part( 'template-parts/element', 'page-header' ); ?>
 
 <div id="main" class="main">
-	<div class="container">
+	<!--@todo: Remove commented lines after testing-->
+	<!--<div class="container">-->
+	<?php
+	if ( have_posts() ) {
+		while ( have_posts() ) :
+			the_post();
+			?>
+			<article>
+
+				<section class="pt-8 pt-md-11">
+					<div class="container">
+						<div class="row justify-content-center">
+							<div class="col-12 col-md-10 col-lg-9 col-xl-8">
+								<?php if ( has_post_thumbnail() ) : ?>
+									<figure class="figure mb-7">
+
+										<!-- Image -->
+										<img class="figure-img img-fluid rounded lift lift-lg"
+											 src="<?php echo esc_attr( get_the_post_thumbnail_url() ); ?>">
+
+										<!-- Caption -->
+										<figcaption class="figure-caption text-center">
+											<?php echo esc_html( get_the_post_thumbnail_caption() ); ?>
+										</figcaption>
+
+									</figure>
+								<?php endif; ?>
+								<!-- Heading -->
+
+								<?php echo the_title( '<h1 class="display-4 text-center">', '</h1>' ); // phpcs:ignore
+								?>
+
+							</div>
+						</div> <!-- / .row -->
+					</div> <!-- / .container -->
+				</section>
+
+				<!-- Content -->
+				<?php get_template_part( 'template-parts/element', 'page-content' ); ?>
+
+			</article>
 		<?php
-		if ( have_posts() ) {
-			while ( have_posts() ) :
-				the_post();
-				?>
-				<article>
-
-					<section class="pt-8 pt-md-11">
-						<div class="container">
-							<div class="row justify-content-center">
-								<div class="col-12 col-md-10 col-lg-9 col-xl-8">
-									<?php if ( has_post_thumbnail() ) : ?>
-										<figure class="figure mb-7">
-
-											<!-- Image -->
-											<img class="figure-img img-fluid rounded lift lift-lg"
-												 src="<?php echo esc_attr( get_the_post_thumbnail_url() ); ?>">
-
-											<!-- Caption -->
-											<figcaption class="figure-caption text-center">
-												<?php echo esc_html( get_the_post_thumbnail_caption() ); ?>
-											</figcaption>
-
-										</figure>
-									<?php endif; ?>
-									<!-- Heading -->
-
-									<?php echo the_title( '<h1 class="display-4 text-center">', '</h1>' ); // phpcs:ignore
-									?>
-
-								</div>
-							</div> <!-- / .row -->
-						</div> <!-- / .container -->
-					</section>
-
-					<!-- Content -->
-					<?php get_template_part( 'template-parts/element', 'page-content' ); ?>
-
-				</article>
-			<?php
-			endwhile;
-		}
-		?>
-	</div><!-- / .container -->
+		endwhile;
+	}
+	?>
+	<!--</div>--><!-- / .container -->
 </div> <!-- / .main -->
 
 <?php get_footer(); ?>
--- single-post.php
+++ single-post.php
@@ -5,59 +5,58 @@
  * @package wpchill-theme
  */
 
- /*
+/*
 * Template Name: Single Post Template
 */
 get_header(); ?>
 <?php get_template_part( 'template-parts/element', 'page-header' ); ?>
 
 <div id="main" class="main">
-	<div class="container">
-		<?php
-		if ( have_posts() ) {
-			while ( have_posts() ) :
-				the_post();
-				?>
-		<article>
+	<!--@todo: Remove commented lines after testing-->
+	<!--<div class="container">-->
+	<?php
+	if ( have_posts() ) {
+		while ( have_posts() ) :
+			the_post();
+			?>
+			<article>
 
-			<section class="pt-8 pt-md-11">
-				<div class="container">
-					<div class="row justify-content-center">
-						<div class="col-12 col-md-10 col-lg-9 col-xl-8">
-						<?php if ( has_post_thumbnail() ) : ?>
-						<figure class="figure mb-7">
+				<section class="pt-8 pt-md-11">
+					<div class="container">
+						<div class="row justify-content-center">
+							<div class="col-12 col-md-10 col-lg-9 col-xl-8">
+								<?php if ( has_post_thumbnail() ) : ?>
+									<figure class="figure mb-7">
 
-							<!-- Image -->
-							<img class="figure-img img-fluid rounded lift lift-lg" src="<?php //echo esc_attr( get_the_post_thumbnail_url() ); ?>" >
+										<!-- Image -->
+										<img class="figure-img img-fluid rounded lift lift-lg"
+											 src="<?php //echo esc_attr( get_the_post_thumbnail_url() ); ?>">
 
-							<!-- Caption -->
-							<figcaption class="figure-caption text-center">
-								<?php echo esc_html( get_the_post_thumbnail_caption() ); ?>
-							</figcaption>
+										<!-- Caption -->
+										<figcaption class="figure-caption text-center">
+											<?php echo esc_html( get_the_post_thumbnail_caption() ); ?>
+										</figcaption>
 
-						</figure>
-						<?php endif; ?>
-							<!-- Heading -->
+									</figure>
+								<?php endif; ?>
+								<!-- Heading -->
 
-							<?php echo the_title( '<h1 class="display-4 text-center">', '</h1>' ); ?>
-							<?php wpchill_base_theme_author(true); ?>
+								<?php echo the_title( '<h1 class="display-4 text-center">', '</h1>' ); ?>
+								<?php wpchill_base_theme_author( true ); ?>
 
-						</div>
-					</div> <!-- / .row -->
-				</div> <!-- / .container -->
-			</section>
-
+							</div>
+						</div> <!-- / .row -->
+					</div> <!-- / .container -->
+				</section>
 
 				<?php get_template_part( 'template-parts/element', 'post-content' ); ?>
 
-		</article>
-
-	</div>
+			</article>
+		<?php
+		endwhile;
+	}
+	?>
+	<!--</div>-->
 </div>
-				<?php
-			endwhile;
-		};
-		?>
-
 
 <?php get_footer(); ?>
